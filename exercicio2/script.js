ee0617234a533e14dda8ddb2b920ca53ff59035d
Kelvin Cristian <christiangamesoficial@gmail.com>
2022-08-03 23:19:10 -0300

    a012-condicionais-I concluido

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,9 +1,42 @@
-let idade;
-let isMaiorIdade;
-let isCursandoOutraFaculdade;
+// # Exercício 2
 
-if(){
+// Escreva um código que receba três valores. Uma idade, um booleano que responda se a pessoa terminou ou não o ensino médio, e um booleano que responda se a pessoa está cursando alguma faculdade.
 
-}else{
-	
-}
+let idade = 18 ;
+let terminouEnsMedio = false ;
+let estaCursandoOutraFaculdade = true ;
+
+// Crie um `if` para cada variável, checando as seguintes afirmacões:
+// - Se a pessoa tem 18 anos ou mais;
+// - Se a pessoa terminou o ensino médio;
+// - Se a pessoa NÃO está cursando alguma faculdade;
+
+if(idade >= 18){
+console.log("Sou maior de idade")
+} else{
+    console.log("É menor de idade")
+}if (terminouEnsMedio){
+console.log("Terminou ensino médio")
+} if(estaCursandoOutraFaculdade) {
+    console.log("Esta cursando outra faculdade")
+}else{console.log("Ensino médio incompleto")
+} 
+
+
+// Crie um `if` que imprima que a afirmacão é verdadeira, e um `else` para imprimir que a afirmacão não é verdadeira.
+
+// <details>
+
+// <summary>Exemplo</summary>
+
+// Caso a primeira pessoa tenha idade >=18:
+
+// <b>"A pessoa é maior de idade"</b>
+
+// e caso não seja:
+
+// <b>"A pessoa é menor de idade"</b>
+
+// </details>
+
+
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,30 @@
-let nacionalidade //= adicione o prompt aqui
+// # Exercício 3
+
+// Crie um código que receba um **prompt** que diga: "Escreva aqui sua nacionalidade" e guarde o valor da resposta em uma const `nacionalidade`.
+
+let nacionalidade = prompt("Qual sua nacionalidade?")
+
+// Vamos considerar que nosso programa aceita as seguintes 5 nacionalidades:
+// - brasileira;
+// - argentina;
+// - uruguaia;
+// - chilena;
+// - colombiana;
+if (nacionalidade === "brasileira"){
+    console.log("Você é Brasileiro");
+}else if (nacionalidade ==="argentina"){
+    console.log("Você é da Argentina");}
+else if (nacionalidade === "uruguaia"){
+    console.log("Você é do Uruguai");
+}else if (nacionalidade === "chilena"){
+    console.log("Você é do Chile");
+}else if (nacionalidade === "colombiana"){
+    console.log("Você é do Chile");
+}else {alert("Nacionalidade não encontrada")}
+// Crie uma estrutura de `if/else if/else` que verifique se a nacionalidade guardada na const `nacionalidade` é igual a cada uma das nacionalidades acima, e caso seja, imprima a nacionalidade no console. O último `else` deve imprimir "nacionalidade não encontrada" caso o valor de `nacionalidade` não corresponda a nenhum dos valores.
+
+// > 💡 Lembre-se de prestar atenção às letras maiúsculas e minúsculas. 
+
+
+
+
